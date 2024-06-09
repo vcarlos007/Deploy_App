8b49540434f23a9ab37fa5ee844ff74182f391e1
vcarlos007 <156484855+vcarlos007@users.noreply.github.com>
2024-06-09 11:47:46 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Welcome to My Deployed React App!</h1>
+       <p>
+         This is a simple React application deployed to GitHub Pages.
+       </p>
+       <a
+        className="App-link"
+        href="https://github.com/explore"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          Learn about GitHub Pages
+        </a>
+      </header>
+    </div>
+  );
+}
+export default App;
